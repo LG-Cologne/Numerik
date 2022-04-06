a8096e632a71678a45a1015c086393101911d8c7
LG-Cologne <leon.jarosch@web.de>
2022-04-06 08:50:22 +0200

    initial Uebung_3/Aufgabe_1.py

--- Uebungen/Uebung_3/Aufgabe_1.py
+++ Uebungen/Uebung_3/Aufgabe_1.py
@@ -1,3 +1,22 @@
 import numpy as np
 
+def zerlegung(a):
+    lu, p = 0,0
+    return lu, p
+
+
+def permutation(p,x):
+    y = 0
+    return y
+
+
+def vorwaerts(l,x):
+    y= 0
+    return y
+
+
+def rueckwaerts(u,x):
+    y = 0
+    return y
+
 print("is Richtig")
