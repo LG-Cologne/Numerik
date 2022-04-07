d22a13f947202b294e8644658aef78d8b3453ba5
LG-Cologne <leon.jarosch@web.de>
2022-04-07 12:45:56 +0200

    Ue3 Aufgabe_1.py implemented LU zerlegung

--- Uebungen/Uebung_3/Aufgabe_1.py
+++ Uebungen/Uebung_3/Aufgabe_1.py
@@ -1,22 +1,52 @@
 import numpy as np
 
+def addRowFakTimes(A, row1, row2, fak):  # Matrix
+    A[row1] = [a + fak * b for a, b in zip(A[row1], A[row2])]
+
 def zerlegung(a):
-    lu, p = 0,0
-    return lu, p
+    for iter in range(len(a)):
+        if (a[iter][iter] == 0):
+            for row in range(iter + 1, len(a)):
+                if a[row][iter] != 0:
+                    a[[iter, row]] = a[[row,iter]]
+                    break
+
+    for iter in range(len(a)):
+        row = iter + 1
+        while row < len(a): # Werte unter Pivot
+            cur = a[row, iter]
+            pivot = a[iter][iter]
+
+            if cur != 0:
+                fak = -(cur / pivot)
+                addRowFakTimes(a, row, iter, fak)
+                a[row][iter] = fak
+
+            row += 1
+
+    return  a
 
 
-def permutation(p,x):
+def permutation(p, b):
     y = 0
     return y
 
 
-def vorwaerts(l,x):
-    y= 0
-    return y
-
-
-def rueckwaerts(u,x):
+def vorwaerts(l, x):
     y = 0
     return y
 
-print("is Richtig")
+
+def rueckwaerts(u, x):
+    y = 0
+    return y
+
+
+if __name__ == '__main__':
+    a = np.array([[0, 0, 0, 1],
+                  [2, 1, 2, 0],
+                  [4, 4, 0, 0],
+                  [2, 3, 1, 0]])
+
+    # print(a[0,0])
+    print(   zerlegung(a))
