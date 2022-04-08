4bb1c30c063c14b57375a94ba82a55d3115d85f9
LG-Cologne <leon.jarosch@web.de>
2022-04-08 12:50:34 +0200

    Ue3 clean up

--- Uebungen/Uebung_3/Aufgabe_1.py
+++ Uebungen/Uebung_3/Aufgabe_1.py
@@ -1,19 +1,23 @@
 import numpy as np
 
+
 def addRowFakTimes(A, row1, row2, fak):  # Matrix
     A[row1] = [a + fak * b for a, b in zip(A[row1], A[row2])]
 
+
 def zerlegung(a):
+    # einmal durchlaufen und zeilen komplett tauchen. U geht dadurch nicht kaputt
+
     for iter in range(len(a)):
         if (a[iter][iter] == 0):
             for row in range(iter + 1, len(a)):
                 if a[row][iter] != 0:
-                    a[[iter, row]] = a[[row,iter]]
+                    a[[iter, row]] = a[[row, iter]]
                     break
 
     for iter in range(len(a)):
         row = iter + 1
-        while row < len(a): # Werte unter Pivot
+        while row < len(a):  # Werte unter Pivot
             cur = a[row, iter]
             pivot = a[iter][iter]
 
@@ -23,8 +27,7 @@ def zerlegung(a):
                 a[row][iter] = fak
 
             row += 1
-
-    return  a
+    return a
 
 
 def permutation(p, b):
@@ -48,5 +51,10 @@ if __name__ == '__main__':
                   [4, 4, 0, 0],
                   [2, 3, 1, 0]])
 
-    # print(a[0,0])
-    print(   zerlegung(a))
+    test = np.array([[1, 1, 1, 1],
+                     [0, 1, 1, 1],
+                     [0, 0, 1, 1],
+                     [0, 0, 0, 1]])
+
+    LU = zerlegung(test)
+    print(LU)
