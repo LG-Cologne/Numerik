053f46927861e6caaa5e57673d1c4a2eaaeeb5e9
LG-Cologne <leon.jarosch@web.de>
2022-05-16 13:09:20 +0200

    Ue6 little fix Aufgabe_2

--- Uebungen/Uebung_6/Aufgabe_2.py
+++ Uebungen/Uebung_6/Aufgabe_2.py
@@ -28,11 +28,11 @@ def solve(a):
 
 if __name__ == '__main__':
 
-for n in range(5, 10, 20):
-    a = np.zeros((n, n))
-    np.fill_diagonal(a, 2)
+    for n in range(5, 10, 20):
+        a = np.zeros((n, n))
+        np.fill_diagonal(a, 2)
 
-    helper = [-1] * (n - 1)
-    a += np.diagflat(helper, 1)
-    a += np.diagflat(helper, -1)
-    print(a)
+        helper = [-1] * (n - 1)
+        a += np.diagflat(helper, 1)
+        a += np.diagflat(helper, -1)
+        print(a)
